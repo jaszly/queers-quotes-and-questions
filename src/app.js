c89698a50bf943348dbdd567dc177f06d56408cb
Marquis Parks <mparks@Marquiss-MacBook-Pro.local>
2017-11-21 15:33:43 -0800

    fixed merge conflicts

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); <<
+<< << < Updated upstream
 const routes = require('./routes');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use('/', routes);
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index', {
+    title: 'Hey',
+    message: 'Hello there!'
+  })
 })
+
+var scrapQoutes = function (artist) {
+  var url = "http"
+}
 
 const port = process.env.PORT || 3000;
 

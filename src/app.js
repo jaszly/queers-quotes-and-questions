f378eebc8ccc587c74ec67408e42a83fdcc6eab5
James McCormack <thejamesmccormack@gmail.com>
2017-11-20 16:38:39 -0800

    express app setup - added app.js file and initialized npm

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+}

147350a657eac08c35161441d60adc985e1b0705
James McCormack <thejamesmccormack@gmail.com>
2017-11-21 16:28:54 -0800

    add function to cycle through images

--- public/scripts/photos.js
+++ public/scripts/photos.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-var dir = "/images/queerphotos/"; // folder location
-var fileextension = ".jpg"; // image format
-// var fileextension = [".png", ".jpg",];
-var i = "1";
-console.log(dir);
-$(function imageloop(){
- $("<img />").attr('src', dir + i + fileextension ).appendTo(".photo-container");
- if (i==27){
- alert('loaded');
- }
- else{
- i++;
- imageloop();
- };
-       });
+  var dir = "/images/queerphotos/"; // folder location
+  var fileextension = ".jpg"; // image format
+  // var fileextension = [".png", ".jpg",];
+  var i = "1";
+  console.log(dir);
+  $(function imageloop(){
+    $(".photo-container").empty();
+    $("<img />").attr('src', dir + i + fileextension ).appendTo(".photo-container");
+      if (i==20){
+        return;
+      }
+      else {
+        i++;
+        setTimeout(imageloop, 10000);
+      };
+   });
 });
